0b466651b07d7794c402dd104b14783d47eca88e
maheshlawnekar <mdlawnekar@gmail.com>
2024-11-27 16:59:39 +0530

    initial commit

---
+++ src/main/java/com/shubham/springbootdemo/SpringBootDemoApplication.java
@@ -0,0 +1,23 @@
+package com.shubham.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
+
+
+    @GetMapping("/hello")
+    public Map<String, String> hello() {
+        return Map.of("message", "Hello World");
+    }
+}
